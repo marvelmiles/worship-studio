025a461e8de90399737b7d247f76584570f47aa7
marvelmiles <marvellousabidemi2@gmail.com>
2026-07-15 04:04:04 +0100

    commiting leftovers

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.22incd1a4f"
+    "revision": "0.jreb6447rp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
