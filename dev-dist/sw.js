f1638eac1fe1bf3ba739eb3315f4ebf2b3ffe216
marvelmiles <marvellousabidemi2@gmail.com>
2026-08-02 10:12:21 +0100

    stable-version

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.1p90u1bcpq"
+    "revision": "0.qujrt8skcmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
